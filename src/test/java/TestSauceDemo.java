d3b2b716ed4321af73d17871d05189e7e0be1a83
IgorsLevins <igorjlevin@gmail.com>
2022-12-03 13:18:32 +0200

    [master]: Project initial upload

--- src/test/java/TestSauceDemo.java
+++ src/test/java/TestSauceDemo.java
@@ -1,8 +1,11 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lv.acodemy.constans.Generic;
 import lv.acodemy.page_object.InventoryPage;
 import lv.acodemy.page_object.LoginPage;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -22,7 +25,11 @@ public class TestSauceDemo {
 
     @BeforeMethod(description = "Preconditions")
     public void initialize() {
-        driver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+
+        driver = new ChromeDriver(options);
         loginPage = new LoginPage(driver);
         inventoryPage = new InventoryPage(driver);
         driver.get(SAUCE_URL);
